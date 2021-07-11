a16db55ac8f4f50604eb87332b77ef765ac924e8
debajyotidas2017 <debajyotidas2017@gmail.com>
2021-07-11 11:10:49 +0530

    adding _plot_master.py in gaussian_smearing_excitation_energy_spectrum

---
+++ gaussian_smearing_Ex_energy_spectrum/_plot_master.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['axes.labelsize'] = 20
+plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['lines.markersize'] = 5
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 20
